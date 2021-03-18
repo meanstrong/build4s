d3c2960a1de656fd18a46cda93b3b167f21aaf50
meanstrong <pmq2008@gmail.com>
2021-03-18 11:26:26 +0800

    update version

--- setup.py
+++ setup.py
@@ -3,22 +3,20 @@
 
 from setuptools import setup, find_packages
 
+import build4s
+
 
 with open("README.md", encoding="utf-8") as f:
     readme = f.read()
 
-with open("LICENSE", encoding="utf-8") as f:
-    license = f.read()
-
 setup(
     name="build4s",
-    version="1.0.2",
+    version=build4s.__version__,
     packages=find_packages(exclude=["test*"]),
     install_requires=["PyYAML"],
     zip_safe=False,
 
     url="https://github.com/meanstrong/build4s",
-    # license=license,
     description="A build tools for CodeBuild",
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -27,7 +25,7 @@ setup(
     maintainer="pengmingqiang",
     maintainer_email="rockypengchina@outlook.com",
     platforms=['any'],
-
+    license="GPLv3",
     entry_points={
         'console_scripts': [
             'buildcli = build4s.cli:main',
